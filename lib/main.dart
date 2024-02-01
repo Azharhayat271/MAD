764d1ef996556788c4fdd98d1fc5d9f470110ba0
Azhar Hayat <115901212+Azharhayat271@users.noreply.github.com>
2024-02-01 09:26:06 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import "package:flutter/material.dart";
+import "package:gradient/gradient.dart";
+
+// program to show the gradient background
+void main() {
+  runApp(const Gradientclass("Gradient Background"));
+}
